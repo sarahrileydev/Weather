756b0b7f699caab53c458cb1ace37a51759eb67b
sriley <sarahriley.dev@gmail.com>
2019-08-10 19:07:36 -0400

    add geolocation

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => {
+  window.navigator.geolocation.getCurrentPosition(
+    (position) => console.log(position), //first call back or "success" call back, gets called when everything goes as planned
+    (err) => console.log(err)
+  )
+  return <div>Hi there!</div>;
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.querySelector("#root"));
