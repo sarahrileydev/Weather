7ab46e79913a29010983571a720f4100655bbdcb
sriley <sarahriley.dev@gmail.com>
2019-08-10 21:50:16 -0400

    refactor to class component

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,12 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position), //first call back or "success" call back, gets called when everything goes as planned
-    (err) => console.log(err)
-  )
-  return <div>Hi there!</div>;
-};
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      lat: null
+    };
+
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({ lat: position.coords.latitude})
+      }, //first call back or "success" call back, gets called when everything goes as planned
+      err => console.log(err)
+    );
+  }
+
+  render() {
+    return <div>Latitude: {this.state.lat}</div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
