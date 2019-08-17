e1bf132e2c00bbdf479bffd60750984065b9aa98
sriley <sarahriley.dev@gmail.com>
2019-08-17 19:05:07 -0400

    refactor state add getSeason func

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,15 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay"
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      lat: null,
-      errorMessage: ""
-    };
-  }
+  state = {
+    lat: null,
+    errorMessage: ""
+  };
 
   componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
@@ -24,7 +21,7 @@ class App extends React.Component {
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />
     }
 
     return <div>Loading!</div>;
