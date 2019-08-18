27436a05c096dbaa580a9c2f9e62ff69ac6b01cb
sriley <sarahriley.dev@gmail.com>
2019-08-18 19:55:05 -0400

    renderContent function

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -16,7 +16,7 @@ class App extends React.Component {
     );
   }
 
-  render() {
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
     }
@@ -25,7 +25,11 @@ class App extends React.Component {
       return <SeasonDisplay lat={this.state.lat} />;
     }
 
-    return <Spinner message="Please accept location request"/>
+    return <Spinner message="Please accept location request" />;
+  }
+
+  render() {
+    return <div className="border red">{this.renderContent()}</div>;
   }
 }
 
